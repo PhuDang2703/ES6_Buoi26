0dad78d82794768ffb9ed863cf4149631af4d935
PhuDang2703 <dangvanquangphu2703@gmail.com>
2022-12-08 01:37:30 +0700

    final

--- Bài tập ES6 buổi 1/baitapRest/js/main2.js
+++ Bài tập ES6 buổi 1/baitapRest/js/main2.js
@@ -4,13 +4,29 @@ let getEle = (id) => {
 
 average = (...nums) =>{
 let total = 0;
-for (const num of nums) {
+for (num of nums) {
     total += num;
   }
   return total / nums.length;
-
 }
-let diemToan = getEle('inpToan').value;
-let diemLy = getEle('inpLy').value;
-let diemHoa = getEle('inpHoa').value;
+document.getElementById('btnKhoi1').onclick = () => {
+  let diemToan = getEle('inpToan').value*1;
+  let diemLy = getEle('inpLy').value*1;
+  let diemHoa = getEle('inpHoa').value*1;
+  result = average(diemToan, diemLy, diemHoa);
+  document.getElementById('tbKhoi1').innerHTML = result;
+}
+document.getElementById('btnKhoi2').onclick = () => {
+  let diemVan = getEle('inpVan').value*1;
+  let diemSu = getEle('inpSu').value*1;
+  let diemDia = getEle('inpDia').value*1;
+  let diemAnh = getEle('inpEnglish').value*1;
+  result = average(diemVan, diemSu, diemDia, diemAnh);
+  document.getElementById('tbKhoi2').innerHTML = result;
+}
 
+tong = (...numbs) => {
+console.log(numbs)
+}
+
+tong(1,2,3)
